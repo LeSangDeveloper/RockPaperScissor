978414afea8faa61f1695e3b56cc214900c30eeb
LeeShangCyber <lesang541191309@gmail.com>
2020-10-01 13:25:45 +0700

    + Add package config

---
+++ RockPaperScissor/src/main/java/team/backend2/rockpaperscissor/config/ApplicationContextConfig.java
@@ -0,0 +1,13 @@
+package team.backend2.rockpaperscissor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+ 
+@Configuration
+@ComponentScan("team.backend2.rockpaperscissor.*") 
+
+public class ApplicationContextConfig {
+
+}
