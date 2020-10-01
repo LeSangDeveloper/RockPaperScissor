39867eb9db63172a0999a06a16b90c1bce57215b
LeeShangCyber <lesang541191309@gmail.com>
2020-10-01 13:45:04 +0700

    + Edit ApplicationContext

--- RockPaperScissor/src/main/java/team/backend2/rockpaperscissor/config/ApplicationContextConfig.java
+++ RockPaperScissor/src/main/java/team/backend2/rockpaperscissor/config/ApplicationContextConfig.java
@@ -7,7 +7,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  
 @Configuration
 @ComponentScan("team.backend2.rockpaperscissor.*") 
-
+@ComponentScan("team.backend2.rockpaperscissor.controller") 
 public class ApplicationContextConfig {
+    
+	@Bean(name = "viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 
 }
