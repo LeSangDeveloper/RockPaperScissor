f3b504d8798d2ba8cfa3b76dcfed7d290072dc7a
LeeShangCyber <lesang541191309@gmail.com>
2020-10-01 16:21:19 +0700

    + Add SignUp

--- RockPaperScissor/src/main/java/team/backend2/rockpaperscissor/controller/ApplicationController.java
+++ RockPaperScissor/src/main/java/team/backend2/rockpaperscissor/controller/ApplicationController.java
@@ -1,5 +1,6 @@
 package team.backend2.rockpaperscissor.controller;
 
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +9,27 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class ApplicationController {
-
+	
+	ArrayList<String> Players;
+	
     @RequestMapping(method = RequestMethod.GET)
     public String hello(Model model) {
         
         model.addAttribute("greeting", "Hello Spring MVC");
         
-        return "index";
+        return "signup";
+    }
+    
+    @RequestMapping(value = "/signup"  ,method = RequestMethod.GET)
+    public String GetSignUp()
+    {
+    	return "signup";
+    }
+    
+    @RequestMapping(value = "/signup"  ,method = RequestMethod.POST)
+    public String PostSignUp()
+    {
+    	return "index";
     }
     
 }
