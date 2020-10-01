b2a18f6118aed703f3da8c0d346fcc0b1c735b21
LeeShangCyber <lesang541191309@gmail.com>
2020-10-01 15:52:06 +0700

    + set Routing

--- RockPaperScissor/src/main/java/team/backend2/rockpaperscissor/controller/ApplicationController.java
+++ RockPaperScissor/src/main/java/team/backend2/rockpaperscissor/controller/ApplicationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/app")
+@RequestMapping("/")
 public class ApplicationController {
 
     @RequestMapping(method = RequestMethod.GET)
@@ -16,5 +16,5 @@ public class ApplicationController {
         
         return "index";
     }
-	
+    
 }
